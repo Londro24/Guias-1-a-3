cc5e3cbe62b2c1b1ec101bb78055f1638d42c4d7
Londro24 <alejandro.ide.figueroa@gmail.com>
2022-09-15 19:02:57 -0300

    Ejercicio 8-1

---
+++ Guia-1/ejercicio8/target/debug/deps/ejercicio8.d
@@ -0,0 +1,5 @@
+C:\Users\aleja\Documents\GitHub\Guias-1-a-3\Guia-1\ejercicio8\target\debug\deps\ejercicio8.exe: src\main.rs
+
+C:\Users\aleja\Documents\GitHub\Guias-1-a-3\Guia-1\ejercicio8\target\debug\deps\ejercicio8.d: src\main.rs
+
+src\main.rs:
---
+++ Guia-1/ejercicio8/target/debug/ejercicio8.d
@@ -0,0 +1,1 @@
+C:\Users\aleja\Documents\GitHub\Guias-1-a-3\Guia-1\ejercicio8\target\debug\ejercicio8.exe: C:\Users\aleja\Documents\GitHub\Guias-1-a-3\Guia-1\ejercicio8\src\main.rs

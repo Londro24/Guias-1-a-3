623d6803b5d514aa29dc57ac93113027d01510ae
Londro24 <alejandro.ide.figueroa@gmail.com>
2022-09-15 11:56:22 -0300

    a

--- Guia-1/ejercicio2/src/main.rs
+++ Guia-1/ejercicio2/src/main.rs
@@ -1,3 +1,19 @@
+use std::io::stdin;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Ingrese un numero");
+    loop {
+        let mut num: String = String::new();
+        stdin().read_line(&mut num).unwrap();
+        let num: u32 = match num.trim().parse(){
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+    let cuadrado: u32 = num.pow(2);
+    let cubo: u32 = num.pow(3);
+
+    print!("El cuadrafo del numero es {}. Y si cubo es {}.", cuadrado, cubo);
+    break;
+    }
 }
